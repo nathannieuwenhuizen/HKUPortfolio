971b26f74812384b126b91cb281ef249ef6a335c
nathannieuwenhuizen <nathan-san@live.nl>
2018-10-11 13:01:57 +0200

    update

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -37,7 +37,7 @@ module.exports = {
                             loader: 'css-loader',
                             options: {
                                 url: false,
-                                minimize: true,
+                                // minimize: true,
                                 sourceMap: true
                             }
                         },
