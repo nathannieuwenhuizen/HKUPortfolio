a675cb47948299be8aed31ce414cadc751184f7c
nathannieuwenhuizen <nathan-san@live.nl>
2018-11-18 16:28:33 +0100

    adding collab project to website

---
+++ huiswerk/COLLABORATIVE_DESIGN/_settings.php
@@ -0,0 +1,7 @@
+<?php 
+$comment = '<h3>'. 
+'Collaborative Design 29 okt t/m 8 nov  2018 - Richard van Tol'
+.'</h3><br>
+Finished movie<br>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/gDKuymIvqoM" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';  
+?>
